49e72f5cf3231172d41b6e40ed817d71a3317546
Dennis Korkchi <dennis.korkchi@gmail.com>
2015-12-16 17:24:26 +0100

    Using the ruby ! operator to modify inline

--- applyCodeStyle.rb
+++ applyCodeStyle.rb
@@ -20,12 +20,12 @@ Dir.glob(files) do  |swift_file|
   puts "updating file #{swift_file}..."
 
   contents = File.read(swift_file)
-  contents = contents.gsub(/^ +$/, '')              # convert empty lines (eg only spaces) to a single newline
-  contents = contents.gsub(/ +$/ , '')              # right trim line (remove ending whitespaces)
-  contents = contents.gsub(/\n{3,}/, "\n\n")        # squash multiple (3 or more) newlines into 2
-  contents = contents.gsub(/\n{2,}\z/, "\n")        # files should end with 1 newline
-  contents = contents.gsub(/\S\K\n\s*{/, " {\n")    # move starting curly brackets to end of previous line
-  contents = contents.gsub(/;$/, '')                # remove terminating semicolons
+  contents.gsub!(/^ +$/, '')              # convert empty lines (eg only spaces) to a single newline
+  contents.gsub!(/ +$/ , '')              # right trim line (remove ending whitespaces)
+  contents.gsub!(/\n{3,}/, "\n\n")        # squash multiple (3 or more) newlines into 2
+  contents.gsub!(/\n{2,}\z/, "\n")        # files should end with 1 newline
+  contents.gsub!(/\S\K\n\s*{/, " {\n")    # move starting curly brackets to end of previous line
+  contents.gsub!(/;$/, '')                # remove terminating semicolons
 
   File.open(swift_file, "w") { |file| 
   	file.puts contents 
