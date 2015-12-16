2b449e6e94d66e47931af84fd22396105b7f2082
Dennis Korkchi <dennis.korkchi@gmail.com>
2015-12-16 23:28:59 +0100

    bugfix for rule for switch cases

--- applyCodeStyle.rb
+++ applyCodeStyle.rb
@@ -18,18 +18,21 @@ Dir.glob(files) do  |swift_file|
   puts "updating file #{swift_file}..."
 
   contents = File.read(swift_file)
-  contents.gsub!(/^\s+$/, '')                     # convert empty lines (eg only spaces) to a single newline
-  contents.gsub!(/ +$/ , '')                      # right trim line (remove ending whitespaces)
-  contents.gsub!(/\n{3,}/, "\n\n")                # squash multiple (3 or more) newlines into 2
-  contents.gsub!(/\n{2,}\z/, "\n")                # files should end with 1 newline
-  contents.gsub!(/\S\K\n\s*{/, " {\n")            # move starting curly brackets to end of previous line
-  contents.gsub!(/;$/, '')                        # remove terminating semicolons
-  contents.gsub!(/\/\/\K(\S)/, ' \1')             # Single-line comment should start with whitespace
-  contents.gsub!(/^([^\?^\n]+)\K( :)/, ':')       # [colon-whitespace] Colon should have no spaces before it
-  contents.gsub!(/(\S:)([^"\s])/, '\1 \2')        # [colon-whitespace] Colon should have exactly one space after it
-  #contents.gsub!(/func \K([A-Z])/){ $1.downcase } # functions should start with lowercase
-  contents.gsub!(/switch \K\((.+)\)/, '\1')       # Switch expression should not be enclosed within parentheses
-  contents.gsub!(/}(\n[^\n])/, '}'"\n"'\1')       # Function should have at least one blank line after it
+  contents.gsub!(/^\s+$/, '')                                    # convert empty lines (eg only spaces) to a single newline
+  contents.gsub!(/ +$/ , '')                                     # right trim line (remove ending whitespaces)
+  contents.gsub!(/\n{3,}/, "\n\n")                               # squash multiple (3 or more) newlines into 2
+  contents.gsub!(/\n{2,}\z/, "\n")                               # files should end with 1 newline
+  contents.gsub!(/\S\K\n\s*{/, " {\n")                           # move starting curly brackets to end of previous line
+  contents.gsub!(/;$/, '')                                       # remove terminating semicolons
+  contents.gsub!(/\/\/\K(\S)/, ' \1')                            # Single-line comment should start with whitespace
+  contents.gsub!(/^([^\?^\n]+)\K( :)/, ':')                      # [colon-whitespace] Colon should have no spaces before it
+  contents.gsub!(/(\S:)([^"\s])/, '\1 \2')                       # [colon-whitespace] Colon should have exactly one space after it
+  #contents.gsub!(/func \K([A-Z])/){ $1.downcase }               # functions should start with lowercase
+  contents.gsub!(/switch\s*\(([^,\(]+)\)/, 'switch \1')          # Switch expression should not be enclosed within parentheses
+  contents.gsub!(/}(\n[^\n])/, '}'"\n"'\1')                      # Function should have at least one blank line after it
+  contents.gsub!(/{(\n.+func)/, '{'"\n"'\1')                     # Function should have at least one blank line before it
+  
+
 
   File.open(swift_file, "w") { |file| 
     file.puts contents 
